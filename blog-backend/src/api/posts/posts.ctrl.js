be5ebc09fc6c18f25374694034c74185abb8442c
dev-93 <cr000@naver.com>
2020-12-13 02:28:27 +0900

    Object ID 검증

--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -1,5 +1,17 @@
 import Post from "../../models/post";
-// 포스트 작성
+import mongoose from "mongoose";
+
+const { ObjectId } = mongoose.Types;
+
+export const checkObjectId = (ctx, next) => {
+    const { id } = ctx.params;
+    if (!ObjectId.isValid(id)) {
+        ctx.status = 400;
+        return;
+    }
+    return next();
+};
+
 export const write = async (ctx) => {
     const { title, body, tags } = ctx.request.body;
     const post = new Post({
