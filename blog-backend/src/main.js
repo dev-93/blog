9d7d37b97b658048472356044089e93bb19cf502
dev-93 <cr000@naver.com>
2020-12-13 04:03:38 +0900

    페이지네이션 구현

--- blog-backend/src/main.js
+++ blog-backend/src/main.js
@@ -3,6 +3,7 @@ import Koa from "Koa";
 import Router from "koa-router";
 import bodyParser from "koa-bodyParser";
 import mongoose from "mongoose";
+
 import api from "./api";
 
 // process.env 내부 값에 대한 레퍼런스 생성
@@ -11,7 +12,7 @@ const { PORT, MONGO_URI } = process.env;
 mongoose
     .connect(MONGO_URI, { useNewUrlParser: true, useFindAndModify: false })
     .then(() => {
-        console.log("Succed Connecting");
+        console.log("Succed Connecting MongDB");
     })
     .catch((e) => {
         console.log(e);
