c23e9a0a051f90b535afdb143ba6fdbce9b2b15e
dev-93 <cr000@naver.com>
2020-12-14 02:33:52 +0900

    회원인증 API 생성 setting

--- blog-backend/src/models/user.js
+++ blog-backend/src/models/user.js
@@ -1,10 +1,28 @@
-import { string } from "@hapi/joi";
-import mongoose, { mongo, Schema } from "mongoose";
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import { function } from "@hapi/joi";
 
 const UserSchema = new Schema({
     username: String,
     hashedPassword: String,
 });
+
+// 인스턴스 메서드
+UserSchema.methods.setPassword = async function (password) {
+    const hash = await bcrypt.hash(password, 10);
+    this.hashedPassword = hash;
+};
+
+UserSchema.methods.checkPassword = async function (password) {
+    const result = await bcrypt.compare(password, this.hashedPassword);
+    result;
+};
+
+// 스태틱 메서드
+
+UserSchema.statics.findByUsername = function (username) {
+    return this.findOne({ username });
+};
 
 const User = mongoose.model("User", UserSchema);
 export default User;
