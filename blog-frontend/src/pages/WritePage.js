17f068a191691fa017b59c47bdb926b5776a92df
dev-93 <cr000@naver.com>
2020-12-20 23:04:35 +0900

    Rotue 셋팅

---
+++ blog-frontend/src/pages/WritePage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const WrtiePage = () => {
+    return <div>쓰기</div>;
+};
+
+export default WrtiePage;
---
+++ blog-frontend/src/pages/index.js
@@ -0,0 +1,7 @@
+import LoginPage from "./LoginPage";
+import PostListPage from "./PostListPage";
+import PostPage from "./PostPage";
+import RegisterPage from "./RegisterPage";
+import WritePage from "./WritePage";
+
+export { LoginPage, PostListPage, PostPage, RegisterPage, WritePage };
