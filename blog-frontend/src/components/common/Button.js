17eb43366e440dbde365e4bcc99f211d41c89f88
dev-93 <cr000@naver.com>
2020-12-20 23:28:38 +0900

    스타일 적용

--- blog-frontend/src/components/common/Button.js
+++ blog-frontend/src/components/common/Button.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import palette from "../../lib/styles/palette";
 
 const Button = (props) => {
     return <StyledButton {...props} />;
