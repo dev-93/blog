4f212754ad6a79beaeb394206d7bbbc92fcad289
dev-93 <cr000@naver.com>
2020-12-28 23:59:57 +0900

    추가

--- blog-frontend/src/containers/auth/RegisterForm.js
+++ blog-frontend/src/containers/auth/RegisterForm.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { changeField, initializeForm, register } from "../../modules/auth";
 import AuthForm from "../../components/auth/AuthForm";
@@ -6,6 +6,7 @@ import user, { check } from "../../modules/user";
 import {withRouter} from 'react-router-dom';
 
 const RegisterForm = ({history}) => {
+    const [error, setError] = useState(null);
     const dispatch = useDispatch();
     const { form, auth, authError } = useSelector(({auth, user}) => ({
         form: auth.register,
@@ -30,7 +31,14 @@ const RegisterForm = ({history}) => {
     const onSubmit = e => {
         e.preventDefault();
         const { username, password, passwordConfirm} = form;
+        if([username, password, passwordConfirm].includes('')) {
+            setError('빈칸을 모두 입력하세요.');
+            return;
+        }
         if(password !== passwordConfirm) {
+            setError('비밀번호가 일치하지 않습니다');
+            dispatch(changeField({form: 'register', key: 'password', value: ''}));
+            dispatch(changeField({form: 'register', key: 'passwordConfirm', value: ''}));
             return;
         }
         dispatch(register({ username, password }));
@@ -43,19 +51,25 @@ const RegisterForm = ({history}) => {
 
     useEffect(() => {
         if (authError) {
-            console.log("오류 발생");
-            console.log(authError);
+            if(authError.response.status === 409) {
+                setError('이미 존재하는 계정명입니다');
+                return;
+            }
+            setError('회원가입 실패');
             return;
-        } 
+        }
+      
         if (auth) {
             console.log("회원가입 성공");
-            console.log(authError);
+            console.log(auth);
             dispatch(check());
         }
     }, [auth, authError, dispatch]);
 
     useEffect(() => {
         if (user) {
+            console.log('회원가입 성공');
+            console.log(user);
             history.push('/');
         }
     }, [history, user]);
@@ -66,6 +80,7 @@ const RegisterForm = ({history}) => {
             form={form}
             onChange={onChange}
             onSubmit={onSubmit}
+            error={error}
         />
     );
 };
