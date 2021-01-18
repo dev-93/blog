89f7643cdfdbaf0a7ce709dd5d1813c96342a614
dev-93 <cr000@naver.com>
2021-01-18 23:51:01 +0900

    tagBox add

--- blog-frontend/src/pages/WritePage.js
+++ blog-frontend/src/pages/WritePage.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Responsive from "../components/common/Responsive";
 import Editor from "../components/write/Editor";
+import TagBox from "../components/write/TagBox";
 
 const WrtiePage = () => {
-    return <Responsive>
-        <Editor />
-    </Responsive>;
+    return (
+        <Responsive>
+            <Editor />
+            <TagBox />
+        </Responsive>);
 };
 
 export default WrtiePage;
