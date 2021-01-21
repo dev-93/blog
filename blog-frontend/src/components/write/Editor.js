69f08454d9aebb516da8284d8c7df719b11d01d5
dev-93 <cr000@naver.com>
2021-01-22 01:47:36 +0900

    리덕스로 글쓰기 상태 관리

--- blog-frontend/src/components/write/Editor.js
+++ blog-frontend/src/components/write/Editor.js
@@ -5,7 +5,7 @@ import 'quill/dist/quill.bubble.css';
 import palette from "../../lib/styles/palette"
 import Responsive from '../common/Responsive';
 
-const Editor = () => {
+const Editor = ({ title, body, onChangeField}) => {
     const quillElement = useRef(null);
     const quillInstance = useRef(null);
 
@@ -22,11 +22,25 @@ const Editor = () => {
                 ],
             },
         });
-    }, []);
+        const quill = quillInstance.current;
+        quill.on('text-change', (delta, oldDelta, source) => {
+            if(source === "user") {
+                onChangeField({key: 'body', value: quill.root.innerHTML});
+            }
+        });
+    }, [onChangeField]);
+
+    const onChangeTitle = (e) => {
+        onChangeField({key: 'title', value: e.target.value});
+    };
 
     return (
         <EditorBlock>
-            <TitleInput placeholder="제목을 입력하세요" />
+            <TitleInput 
+                placeholder="제목을 입력하세요" 
+                onChange={onChangeTitle} 
+                value={title}
+            />
             <QuillWrapper>
                 <div ref={quillElement}/>
             </QuillWrapper>
