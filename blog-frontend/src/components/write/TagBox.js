d71d044506374149a8cdfcee8f36b105f6fd41ee
dev-93 <cr000@naver.com>
2021-01-21 23:59:50 +0900

    개발 중

--- blog-frontend/src/components/write/TagBox.js
+++ blog-frontend/src/components/write/TagBox.js
@@ -1,18 +1,54 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import styled from 'styled-components';
 import palette from '../../lib/styles/palette';
 
-const TagItem = React.memo(({tag})=> <Tag>#{tag}</Tag>);
+// tag값이 바뀔 때만 리렌더링되도록 처리
+const TagItem = React.memo(({tag, onRemove})=> ( 
+    <Tag onClick={() => onRemove(tag)}>#{tag}</Tag>
+));
 
-const TagList = React.memo(({tags})=> (
+// tags값이 바뀔 때만 리렌더링되도록 처리
+const TagList = React.memo(({tags, onRemove})=> (
     <TagListBlock>
         {tags.map((tag) => (
-            <TagItem ket={tag} tag={tag} />
+            <TagItem ket={tag} tag={tag} onRemove={onRemove}/>
         ))}
     </TagListBlock>
 ));
 
 const TagBox = () => {
+    const [input, setInput] = useState('');
+    const [localTags, setLocalTags] = useState([]);
+
+    const insertTag = useCallback(
+        tag => {
+            if (!tag) return;
+            if (localTags.includes(tag)) return;
+            setLocalTags([...localTags, tag]);
+        },
+        [localTags],
+    );
+
+    const onRemove = useCallback(
+        tag => {
+            setLocalTags(localTags.filter(t => t!== tag));
+        }, 
+        [localTags],
+    );
+
+    const onChange = useCallback(e => {
+        setInput(e.target.value);
+    }, []);
+
+    const onSubmit = useCallback(
+        e => {
+            e.preventDefault();
+            insertTag(input.trim());
+            setInput('');
+        }, 
+        [input, insertTag],
+    );
+
     return (
         <TagBoxBlock>
             <h4>태그</h4>
